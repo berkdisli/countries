c5ecf47a47b2e08458ef9aeaebfac6b8816af68d
Berk Disli <berkdisli@yahoo.com>
2023-03-13 21:19:07 +0100

    delete unnecessary files

--- src/components/navbar/Menu.tsx
+++ src/components/navbar/Menu.tsx
@@ -22,19 +22,10 @@ const Menu = ({isMenuOpen =false, value, placeholder, options, onChange}: MenuPr
     <div className={"menu " + (isMenuOpen && "active")}>
       <ul>
         <li onClick={() => isMenuOpen}>
-          <a href="#intro">Home</a>
         </li>
         <li onClick={() => isMenuOpen}>
-          <a href="#portfolio">Portfolio</a>
         </li>
         <li onClick={() => isMenuOpen}>
-          <a href="#works">Works</a>
-        </li>
-        <li onClick={() => isMenuOpen}>
-          <a href="#contact">Contact</a>
-        </li>
-        <li onClick={() => isMenuOpen}>
-          <a href="#impressum">Impressum</a>
         </li>
       </ul>
     </div>
