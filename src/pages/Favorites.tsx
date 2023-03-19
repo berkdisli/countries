c83a12fafc3a2ca45c84a289bf7076b1b6bb4676
Berk Disli <berkdisli@yahoo.com>
2023-03-19 10:11:48 +0100

    fav-back, mobile css

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { HiArrowNarrowLeft } from "react-icons/hi";
 
 import { useAppSelector } from "../app/hooks";
+import Button from "../components/Button";
+
 
 const Favorites = () => {
   const favorites = useAppSelector(
     (state) => state.countriesR.favoriteCountries
   );
+  const navigate = useNavigate();
+
   const countries = useAppSelector((state) => state.countriesR.countries);
 
   const favoriteInfo: any = countries.filter((country) =>
     favorites.includes(country.cca3)
   );
+
+  const handleBackClick = (): void => {
+    navigate(-1);
+  };
 
   const favoriteList = favoriteInfo.map((country: any) => (
     <Link to={`/detail/${country.cca3.toLowerCase()}`}>
@@ -22,6 +32,12 @@ const Favorites = () => {
     <div className="favorite-body">
       <h1>Favorites</h1>
       {favoriteList}
+      <div className="action-buttons">
+        <Button onClick={handleBackClick}>
+          <HiArrowNarrowLeft size="1.5rem" />
+          <span>Back</span>
+        </Button>
+      </div>
     </div>
   );
 };
